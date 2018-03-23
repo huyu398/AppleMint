48010d008f8ed12fe9437a1ec18af5322aa2b02e
huyu398 <huyu.sakuya4645@gmail.com>
2018-03-23 18:56:40 +0900

    [WIP] Now coding answer of problem016

---
+++ workspace/project_euler/problem016/problem016.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+log_num = 15 * np.log10(2)
+digit = int(np.ceil(log_num))
+digits = []
+for d in reversed(range(1, digit+1)):
+
+from IPython import embed; embed()
